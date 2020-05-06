bd4196a52582cbb6cc6eb3b11d415a10bc4b597b
PRIYAGRAJ2610 <56988908+PRIYAGRAJ2610@users.noreply.github.com>
2020-05-06 17:39:30 +0530

    first commit

---
+++ mahabharatquiz.py
@@ -0,0 +1,70 @@
+#this a mahabharat quiz.
+print ("this ia a simple  mahabharatquiz coded by >>>>>> prs   :   \n  ")
+print(" this quiz consist of 10 questions.")
+print(" *************       mahabharat quiz      ************ :")
+a = input(" question 1 who is famous as dharamraj in mahabharat \n ")
+b = input (" question 2 who is the yuvraj of that state where pandavas survive during their agyatvaas \n")
+c = input(" question 3 what is the name of pandava's mother  \n")
+d = input(" question 4 what is the name of abhimanyu's mother in mahabharat \n")
+e = input(" question 5 who is the father of bhisma pitamah  \n")
+f = input("who was king shantanu's first wife \n")
+g = input("who did duryodhana crown as king of anga \n")
+h = input("who was karna's charioteer in the war \n")
+i = input("who is the real father of arjun \n ")
+j = input("who killed dushasana \n")
+if a == "yudhistir" or a == "yudhisthir" : 
+   print(" answer 1 is correct : \n ",a)
+else :
+    print("answer 1 is incorrect right answer is yudhistir")
+print("*********************************************************************")
+if b == "uttar kumar" or b == "uttar" :
+    print(" answer 2 is correcct " , b)
+else :
+    print("answer 2 is incorrect  right answer is uttar kumar") 
+print("**********************************************************************")
+if c == "kunti" :
+    print(" answer 3 is correct : \n " , c)
+else :
+    print("answer 3 is incorrect right answer is kunti ")
+print("*********************************************************************")
+if d == "subhadra" or d == "subhaddra" :
+    print("answer 4 is correct : \n ",d)
+else :
+    print("answer 4 is incorrect right answer is subhadra ")
+print("**********************************************************************")
+if e == "shantanu" or e == "shantnu" or e == "santanu"  or e == "santnu" :
+    print("answer 5 is correct : \n ",e)
+else :
+    print("answer 5 is incorrect right answer is shantanu")
+print("**********************************************************************")
+if f == "ganga" or f == "gnga" :
+    print("answer  6 is correct : \n ",f)
+else :
+    print("answer 6 is incorrect right answer is ganga")
+print("**********************************************************************")
+if g == "karna" or g == "karana" :
+    print("answer 7 is correct : \n ",g)
+else :
+    print("answer 7 is incorrect right answer is karna")
+print("**********************************************************************")
+if h == "sanjay" or h == "sanjaya" or h == "sanjya"   :
+    print("answer  8 is correct : \n ",h)
+else :
+    print("answer 8 is incorrect right answer is sanjay")
+print("**********************************************************************")
+if i == "indra" or i == "devraj indra" or i == "dev indra"   :
+    print("answer 9 is correct : \n ",i)
+else :
+    print("answer 9 is incorrect right answer is indra")
+print("**********************************************************************")
+if j == "bheema" or j == "bheem" or j == "bheemsen"  or j == "bhimsen" :
+    print("answer 10 is correct : \n ",j)
+else :
+    print("answer 10 is incorrect right answer is bheem")
+
+
+
+
+
+
+print("**************  thanks for your precious time - by prs ********************")
